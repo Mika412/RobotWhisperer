24f994417840c75aa131c5fb34f164b3e3d0fadf
Mykhaylo Marfeychuk <mykhamarf17@gmail.com>
2025-09-01 00:02:32 +0100

    Add request managing open request

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
+    import MainScreen from "$lib/components/MainScreen.svelte";
     import Sidebar from "$lib/components/sidebar/Sidebar.svelte";
-    import WelcomeScreen from '$lib/components/WelcomeScreen.svelte';
+    import WelcomeScreen from "$lib/components/WelcomeScreen.svelte";
+    import { openItems } from "$lib/stores/workspaceStore";
 </script>
 
-<main class="flex min-h-screen w-full text-text-main font-sans antialiased">
-    <Sidebar />
-
-    <main class="flex-1">
-		<WelcomeScreen />
-	</main>
+<main
+    class="flex h-screen w-full overflow-hidden text-text-main font-sans antialiased"
+>
+    <div class="flex-none">
+        <Sidebar />
+    </div>
+    <main class="flex-1 w-dvw">
+        {#if $openItems.length === 0}
+            <WelcomeScreen />
+        {:else}
+            <MainScreen />
+        {/if}
+    </main>
 </main>
